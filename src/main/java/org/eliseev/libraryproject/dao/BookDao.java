0353d123e0a6fb6127bf7de260da999281c2c000
Andrey Eliseev <eliseevdry@mail.ru>
2022-07-17 23:20:04 +0300

    create DAO

--- src/main/java/org/eliseev/libraryproject/dao/BookDao.java
+++ src/main/java/org/eliseev/libraryproject/dao/BookDao.java
@@ -1,4 +1,55 @@
 package org.eliseev.libraryproject.dao;
 
+import org.eliseev.libraryproject.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class BookDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT* FROM book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO book(title, author, issue_year) VALUES (?, ?, ?)",
+                book.getTitle(),
+                book.getAuthor(),
+                book.getYear()
+        );
+    }
+
+    public void update(int id, Book book) {
+        jdbcTemplate.update("UPDATE book SET title=?, author=?, issue_year=? WHERE id=?",
+                book.getTitle(),
+                book.getAuthor(),
+                book.getYear(),
+                id
+        );
+    }
+
+    public void setPerson(int id, int personId) {
+        jdbcTemplate.update("UPDATE book SET person_id=? WHERE id=?", personId, id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM book WHERE id=?", id);
+    }
+
+
 }
--- src/main/java/org/eliseev/libraryproject/dao/PersonDao.java
+++ src/main/java/org/eliseev/libraryproject/dao/PersonDao.java
@@ -1,4 +1,49 @@
 package org.eliseev.libraryproject.dao;
 
+import org.eliseev.libraryproject.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT* FROM person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO person(full_name, year_of_birth, address) VALUES (?, ?, ?)",
+                person.getFullName(),
+                person.getYearOfBirth(),
+                person.getAddress()
+        );
+    }
+
+    public void update(int id, Person person) {
+        jdbcTemplate.update("UPDATE person SET full_name=?, year_of_birth=?, address=? WHERE id=?",
+                person.getFullName(),
+                person.getYearOfBirth(),
+                person.getAddress(),
+                id
+        );
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
 }
